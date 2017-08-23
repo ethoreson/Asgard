400e76ee06d1d825471d9b50255b2c49190afab6
Emilie Thoreson <thoresonemilie@gmail.com>
2017-08-23 11:39:40 -0700

    initial hangman html

--- src/app/midgard/midgard.component.ts
+++ src/app/midgard/midgard.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { FormsModule} from '@angular/forms';
 @Component({
   selector: 'app-midgard',
   templateUrl: './midgard.component.html',
   styleUrls: ['./midgard.component.css']
 })
 export class MidgardComponent implements OnInit {
-
+  @Input() lives: number;
+  @Output() clickSender = new EventEmitter();
   constructor() { }
 
   ngOnInit() {
+    var midgardNextDiv = document.getElementById('midgardNextDiv');
+    midgardNextDiv.style.display = 'none';
   }
 
 }
